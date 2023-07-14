93eb5c6d0246a6231cefc33db889c7dbff7fa126
Lawton Stephens <stephens.lawton@gmail.com>
2023-07-14 15:20:21 -0400

    echo server

---
+++ lesson_2/server.rb
@@ -0,0 +1,13 @@
+require "socket"
+
+server = TCPServer.new("localhost", 3003) # server will accept requests sent to localhost, port number 3003
+  loop do 
+    client = server.accept   # waits until request comes in, accepts and returns client object
+
+    request_line = client.gets
+    puts request_line        # prints request to console 
+
+    client.puts request_line  # send back to client so displayed in web browser
+    client.puts rand(500)
+    client.close # close connection
+  end 
